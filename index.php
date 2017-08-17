6e5dd65e390585af47e7c8c60f8f35aed3a6d81b
jmpaddock <jpadd14@u.washington.edu>
2017-08-16 22:24:51 -0700

    03) Create Your Template Files

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <?php get_header(); ?>
-    
-<!-- begin content -->
-    <div id="content">
+    <!-- begin content -->
+    <div class="content">
         
-        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <?php the_content(''); ?>  
-        
+         <?php if(have_posts()) : while (have_posts()) : the_post(); ?>
+            <article id-"article-<?php the_ID(); ?>" class="article">
+                <h2><a href="<?php the_permalink(); ?>"<?php the_title(); ?></a></h2>
+                <small>Posted by <?php the_author(); ?> on the <?php the_time('F jS, Y'); ?> in the <?php the_category(', '); ?></small>
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+                <p></p><?php get_the_excerpt(); ?><a href="<?php the_permalink(); ?> Full Story&nbsp;&raquo;</a></p>
+            </article>
         <?php endwhile; endif; ?>
         
-<small>index.php</small>  
+        <small>index.php</small>        
     </div>
-            
-        
-<!-- end content -->
-
-<!-- Begin main menu -->
-<?php wp_nav_menu(array('theme_location' => 'main_menu', 'container' => 'div', 'container_id' => 'navigation',)); ?>
-<!-- end main menu -->
-
-<!-- sidebar -->
-<?php get_sidebar(); ?>
-<!-- end sidebar -->
+    <!-- end content --> 
+    
 
 <?php get_footer(); ?>
